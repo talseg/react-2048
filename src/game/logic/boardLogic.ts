2fe38c1775eaa1bc4defeb07c1ca637e4450186a
TalSegal <talse@DESKTOP-G6D799G>
2025-08-19 19:52:49 +0300

    Left, Right Up Down Moves

--- src/game/logic/boardLogic.ts
+++ src/game/logic/boardLogic.ts
@@ -14,6 +14,15 @@ export const getNewMatrixByDirection = (matrix: number[][], direction: Direction
                 [0,0,0,0]
             ]);
         }
+
+        if (matrix[3][3] === 2) {
+            return ([
+                [0,0,0,0],
+                [0,0,0,0],
+                [0,0,0,0],
+                [2,0,0,0]
+            ]);
+        }
         break;
 
         case "right" :
@@ -25,6 +34,60 @@ export const getNewMatrixByDirection = (matrix: number[][], direction: Direction
                 [0,0,0,0]
             ]);
         }
+
+        if (matrix[3][0] === 2) {
+            return ([
+                [0,0,0,0],
+                [0,0,0,0],
+                [0,0,0,0],
+                [0,0,0,2]
+            ]);
+        }
+        break;
+
+        case "up":
+            if(matrix[3][0]==2){
+
+                return ([
+                [2,0,0,0],
+                [0,0,0,0],
+                [0,0,0,0],
+                [0,0,0,0]
+                ]);
+            }
+
+            if(matrix[3][3]==2){
+
+                return ([
+                [0,0,0,2],
+                [0,0,0,0],
+                [0,0,0,0],
+                [0,0,0,0]
+                ]);
+            }
+        break;
+        
+        case "down":
+
+        if(matrix[0][0]==2){
+
+                return ([
+                [0,0,0,0],
+                [0,0,0,0],
+                [0,0,0,0],
+                [2,0,0,0]
+                ]);
+            }
+        
+        if(matrix[0][3]==2){
+
+                return ([
+                [0,0,0,0],
+                [0,0,0,0],
+                [0,0,0,0],
+                [0,0,0,2]
+                ]);
+        } 
         break;
     }
     
