1b5c749282f0e3cd990dbee248f87bcbbf936350
TalSegal <talse@DESKTOP-G6D799G>
2025-08-20 11:50:03 +0300

    continue

--- src/game/logic/boardLogic.ts
+++ src/game/logic/boardLogic.ts
@@ -9,85 +9,54 @@ export const getNewMatrixByDirection = (matrix: number[][], direction: Direction
     switch (direction) {
 
         case "left" :
-        if (matrix[0][3] === 2) {
+        if (newMatrix[0][3] === 2) {
             newMatrix[0][3] = 0;
             newMatrix[0][0] = 2;
         } 
-        else if (matrix[3][3] === 2) {
-            return ([
-                [0,0,0,0],
-                [0,0,0,0],
-                [0,0,0,0],
-                [2,0,0,0]
-            ]);
+        if (matrix[3][3] === 2) {
+            newMatrix[3][3] = 0;
+            newMatrix[3][0] = 2;
         }
         break;
 
         case "right" :
         if (matrix[0][0] === 2) {
-            return ([
-                [0,0,0,2],
-                [0,0,0,0],
-                [0,0,0,0],
-                [0,0,0,0]
-            ]);
+            newMatrix[0][0] = 0;
+            newMatrix[0][3] = 2;
         }
 
         if (matrix[3][0] === 2) {
-            return ([
-                [0,0,0,0],
-                [0,0,0,0],
-                [0,0,0,0],
-                [0,0,0,2]
-            ]);
+            newMatrix[3][0] = 0;
+            newMatrix[3][3] = 2;
         }
         break;
 
         case "up":
             if(matrix[3][0]==2){
-
-                return ([
-                [2,0,0,0],
-                [0,0,0,0],
-                [0,0,0,0],
-                [0,0,0,0]
-                ]);
+                newMatrix[3][0] = 0;
+                newMatrix[0][0] = 2;
             }
 
             if(matrix[3][3]==2){
-
-                return ([
-                [0,0,0,2],
-                [0,0,0,0],
-                [0,0,0,0],
-                [0,0,0,0]
-                ]);
+                newMatrix[3][3] = 0;
+                newMatrix[0][3] = 2;
             }
         break;
         
         case "down":
 
         if(matrix[0][0]==2){
-
-                return ([
-                [0,0,0,0],
-                [0,0,0,0],
-                [0,0,0,0],
-                [2,0,0,0]
-                ]);
-            }
+            newMatrix[0][0] = 0;
+            newMatrix[3][0] = 2;
+        }
         
         if(matrix[0][3]==2){
-
-                return ([
-                [0,0,0,0],
-                [0,0,0,0],
-                [0,0,0,0],
-                [0,0,0,2]
-                ]);
+            newMatrix[0][3] = 0;
+            newMatrix[3][3] = 2;
         } 
         break;
     }
     
     return newMatrix;
 }
+
