27513b1fe2d2b96ceda34e8fc8b6b829fc14b8ca
TalSegal <talse@DESKTOP-G6D799G>
2025-08-20 12:11:51 +0300

    cont

--- src/game/logic/boardLogic.ts
+++ src/game/logic/boardLogic.ts
@@ -2,75 +2,77 @@ import { mapMatrix, printMatrix } from "./matrixUtils";
 
 export type Direction = "left" | "right" | "up" | "down";
 
-export const getNewMatrixByDirection = (matrix: number[][], direction: Direction): number[][] => {
-
-    let newMatrix: number[][] = mapMatrix(matrix);
+export const getNewMatrixByDirection = (board: number[][], direction: Direction): number[][] => {
+    
+    let newBoard: number[][] = mapMatrix(board);
 
     switch (direction) {
 
         case "left":
-            handelLeft(newMatrix);
+                    handelLeft(newBoard);
             break;
 
         case "right":
-            handelRight(newMatrix);
+                            handelRight(newBoard);
             break;
 
         case "up":
-            handelUp(newMatrix);
+            handelUp(newBoard);
             break;
 
         case "down":
-            handelDown(newMatrix);
+            handelDown(newBoard);
             break;
     }
 
-    return newMatrix;
+    return newBoard;
 }
 
-const handelLeft = (matrix: Number[][]) => {
-    if (matrix[0][3] === 2) {
-        matrix[0][3] = 0;
-        matrix[0][0] = 2;
+const handelLeft = (board: Number[][]) => {
+    if (board[0][3] === 2) {
+        board[0][3] = 0;
+        board[0][0] = 2;
     }
-    if (matrix[3][3] === 2) {
-        matrix[3][3] = 0;
-        matrix[3][0] = 2;
-    }
-}
-
-const handelRight = (matrix: Number[][]) => {
-    if (matrix[0][0] === 2) {
-        matrix[0][0] = 0;
-        matrix[0][3] = 2;
-    }
-
-    if (matrix[3][0] === 2) {
-        matrix[3][0] = 0;
-        matrix[3][3] = 2;
-    }
-}
-const handelUp = (matrix: Number[][]) => {
-    if (matrix[3][0] == 2) {
-        matrix[3][0] = 0;
-        matrix[0][0] = 2;
-    }
-
-    if (matrix[3][3] == 2) {
-        matrix[3][3] = 0;
-        matrix[0][3] = 2;
+    if (board[3][3] === 2) {
+        board[3][3] = 0;
+        board[3][0] = 2;
     }
 }
 
-const handelDown = (matrix: Number[][]) => {
-    if (matrix[0][0] == 2) {
-        matrix[0][0] = 0;
-        matrix[3][0] = 2;
+const handelRight = (board: Number[][]) => {
+    if (board[0][0] === 2) {
+        board[0][0] = 0;
+        board[0][3] = 2;
     }
 
-    if (matrix[0][3] == 2) {
-        matrix[0][3] = 0;
-        matrix[3][3] = 2;
+    if (board[3][0] === 2) {
+        board[3][0] = 0;
+        board[3][3] = 2;
+    }
+}
+const handelUp = (board: Number[][]) => {
+    if (board[3][0] == 2) {
+        board[3][0] = 0;
+        board[0][0] = 2;
+    }
+
+    if (board[3][3] == 2) {
+        board[3][3] = 0;
+        board[0][3] = 2;
+    }
+}
+
+const handelDown = (board: Number[][]) => {
+
+
+    if (board[0][0] == 2) {
+        board[0][0] = 0;
+        board[3][0] = 2;
+    }
+
+    if (board[0][3] == 2) {
+        board[0][3] = 0;
+        board[3][3] = 2;
     }
 }
 
