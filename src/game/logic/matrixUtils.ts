5d9082a00c751513a4f9c04a5cf03ccc0a24623d
TalSegal <talse@DESKTOP-G6D799G>
2025-08-21 15:18:11 +0300

    tried the print

--- src/game/logic/matrixUtils.ts
+++ src/game/logic/matrixUtils.ts
@@ -14,12 +14,37 @@ export const mapMatrix = (matrix: number[][]): number[][] => {
 
 export const getRow = (matrix: number[][], rowIndex: number): number[] => {
     const row = [];
-
-
-
-
-    return [];
+    for(let col=0; col< matrix[rowIndex].length; col++){
+        row.push(matrix[rowIndex][col]);
+    }
+    return row;
 }
+
+
+// export const printMatrix = (matrix: number[][], header?: string) => {
+
+//     let output = header ? `${header}\n` : "";
+//     for (let row: number = 0; row < matrix.length; row++) {
+//         // for (let col: number = 0; col < matrix[row].length; col++) {
+//         //     output += matrix[row][col] + " ";
+//         // }
+//         printRow(matrix[row],header);
+//         console.log("\n");
+//         //output += "\n";
+//     }
+//     //console.log(output);
+// }
+
+// export const printRow = (row: number[], header?: string) =>{
+
+//         let output = header ? `${header}\n` : "";
+//         for (let index: number = 0; index < row.length; index++) {
+//             output += row[index] + " ";
+//         }
+
+//     console.log(output);
+// }
+
 
 
 export const printMatrix = (matrix: number[][], header?: string) => {
