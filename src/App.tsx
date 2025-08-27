eb5a4d70c113f97454dadcc23d72652ffff839ca
TalSegal <talse@DESKTOP-G6D799G>
2025-08-27 17:15:37 +0300

    fixed the warnings again

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,6 @@
 // import { styled } from 'styled-components';
-import { styled } from 'styled-components';
 import './App.css'
 import { Game } from './game/conponents/Game/Game';
-
-const PageWrapper = styled.div`
-  min-height: 90vh;  
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: start; 
-`;
 
 
 function App() {
