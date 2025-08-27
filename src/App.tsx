4590b88e70e6c02cb3fbed4c8ec207a90ea0590f
TalSegal <talse@DESKTOP-G6D799G>
2025-08-27 14:56:50 +0300

    fix App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,20 @@
 import './App.css'
-import styled from 'styled-components'
-import FullscreenToggle from './game/conponents/fullScreenToggle';
 import { Game } from './game/conponents/Game/Game';
 
-const PageWrapper = styled.div`
-  min-height: 90vh;  
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: start;  
-`;
+// const PageWrapper = styled.div`
+//   min-height: 90vh;  
+//   display: flex;
+//   flex-direction: column;
+//   align-items: center;
+//   justify-content: start;  
+// `;
 
-const InfoWrapper = styled.div`
-  margin-top: 20px;
-  color: #000000;
-  font-size: 20px;
-  font-weight: bold;
-`;
+
 
 function App() {
 
   return (
-    // <PageWrapper>
-
-    //   <FullscreenToggle/>
-    //     <h1 style={{ color: "black" }}>2048</h1>
-
-
       <Game/>
-
-    //   <InfoWrapper>
-    //     {"Game by Inbar and Tal Segal"}
-    //   </InfoWrapper>
-
-    // </PageWrapper>
   )
 }
 
