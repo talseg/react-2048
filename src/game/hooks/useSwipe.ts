01dfc023f38669cfd893dc865de100b70ad924dd
Tal Segal <talseg7@gmail.com>
2025-09-09 18:00:57 +0300

    add use swipe

--- src/game/hooks/useSwipe.ts
+++ src/game/hooks/useSwipe.ts
@@ -1,19 +1,31 @@
 import { useState } from "react";
 import type { Direction } from "../logic/boardLogic";
 
-const SWIPE_DISTANCE = 30;
+const SWIPE_DISTANCE = 20;
 
-interface UseSwipeProps {
+export interface UseSwipeProps {
     onTouchStart: (e: React.TouchEvent) => void;
-    onTouchEnd: (e: React.TouchEvent) => void
+    onTouchEnd: (e: React.TouchEvent) => void;
+    onTouchMove: (e: React.TouchEvent) => void;
 }
 
 export const useSwipe = (onSwipe: (direction: Direction) => undefined): UseSwipeProps => {
 
     const [touchStartX, setTouchStartX] = useState(0);
     const [touchStartY, setTouchStartY] = useState(0);
+    const [isInSwipe, setIsInSwipe] = useState(false);
 
-    const handleTouchEnd = (e: React.TouchEvent) => {
+
+    const handleSwipeEnd = (direction: Direction) => {
+        onSwipe(direction);
+        setIsInSwipe(false);
+    }
+
+    const handleTouchMove = (e: React.TouchEvent) => {
+
+        if (!isInSwipe) 
+            return;
+
         const deltaX = e.changedTouches[0].screenX - touchStartX;
         const deltaY = e.changedTouches[0].screenY - touchStartY;
 
@@ -23,23 +35,48 @@ export const useSwipe = (onSwipe: (direction: Direction) => undefined): UseSwipe
         // X swipe
         if (swipeLengthX > swipeLengthY) {
             if (swipeLengthX > SWIPE_DISTANCE) {
-                if (deltaX < 0) onSwipe("left");
-                else if (deltaX > 0) onSwipe("right");
+                if (deltaX < 0) handleSwipeEnd("left");
+                else if (deltaX > 0) handleSwipeEnd("right");
             }
         }
         // Y Swipe
         else {
             if (swipeLengthY > SWIPE_DISTANCE) {
-                if (deltaY < 0) onSwipe("up");
-                else if (deltaY > 0) onSwipe("down");
+                if (deltaY < 0) handleSwipeEnd("up");
+                else if (deltaY > 0) handleSwipeEnd("down");
             }
         }
+    }
+
+
+    const handleTouchEnd = (/* e: React.TouchEvent */) => {
+        // const deltaX = e.changedTouches[0].screenX - touchStartX;
+        // const deltaY = e.changedTouches[0].screenY - touchStartY;
+
+        // const swipeLengthX = Math.abs(deltaX);
+        // const swipeLengthY = Math.abs(deltaY);
+
+        // // X swipe
+        // if (swipeLengthX > swipeLengthY) {
+        //     if (swipeLengthX > SWIPE_DISTANCE) {
+        //         if (deltaX < 0) onSwipe("left");
+        //         else if (deltaX > 0) onSwipe("right");
+        //     }
+        // }
+        // // Y Swipe
+        // else {
+        //     if (swipeLengthY > SWIPE_DISTANCE) {
+        //         if (deltaY < 0) onSwipe("up");
+        //         else if (deltaY > 0) onSwipe("down");
+        //     }
+        // }
     }
 
     const handleTouchStart = (e: React.TouchEvent) => {
         setTouchStartX(e.changedTouches[0].screenX);
         setTouchStartY(e.changedTouches[0].screenY);
+        setIsInSwipe(true);
     }
 
-    return { onTouchStart: handleTouchStart, onTouchEnd: handleTouchEnd }
+    return { onTouchStart: handleTouchStart, onTouchEnd: handleTouchEnd, onTouchMove: handleTouchMove }
 } 
