1185d740b564ed147faea53decc25acaf4870d2b
Tal Segal <talseg7@gmail.com>
2025-09-15 14:03:15 +0300

    Undo width change

--- src/game/conponents/Game/Game.tsx
+++ src/game/conponents/Game/Game.tsx
@@ -16,15 +16,12 @@ import { IconRestart, IconUndo } from "../../../assets/Icons";
 const VERSION = pkg.version;
 
 const PageWrapper = styled.div`
-  min-height: 100vh;  
+  min-height: 90vh;
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: start;
   background-color: #d9d9d9;
-  min-width: 98vw;
-  margin-left: -28px;
-  margin-top: -28px;
 `;
 
 const InfoWrapper = styled.div`
