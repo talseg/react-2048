eadb21c9450725402350b4c156f4b610caa17678
Tal Segal <talseg7@gmail.com>
2025-10-03 13:44:51 +0300

    Adopt to IOS (#55)
    * update todo
    
    * disable touch actions to prevent swipe down to refresh
    
    * release: bump patch to 2.2.9
    
    * detect IOS
    
    * release: bump patch to 2.2.10
    
    * debug IsIOS
    
    * fix warning
    
    * IOS Adopt
    
    * merge with main

--- src/game/utilities/utilities.ts
+++ src/game/utilities/utilities.ts
@@ -2,5 +2,11 @@ export const isOnIOS = () => {
       if (typeof window === 'undefined' || typeof navigator === 'undefined') {
         return false; // Not in a browser environment
     }
-    return /iPad|iPhone|iPod/.test(navigator.userAgent);
+    return isIPhone() || iSIpadOrMac()
 }
+
+export const isIPhone = () => 
+  navigator.userAgent.includes("iPhone");
+
+export const iSIpadOrMac = 
+  () => navigator.userAgent.includes("Macintosh");
