2e125a29dd77758f28d5edec8de987f31ca3a6f0
Tal Segal <talseg7@gmail.com>
2025-10-21 20:35:39 +0300

    Removed comments

--- src/game/logic/AnimationLogic.ts
+++ src/game/logic/AnimationLogic.ts
@@ -1,76 +1,6 @@
-import { getRowTilesAfterLeftSwipe, type AnimationPlan, type MovingTile } from "./boardLogic";
+import { getRowTilesAfterLeftSwipe, type AnimationPlan } from "./boardLogic";
 import { clockwiseBoardRotation, getRow, horizontalBoardFlip } from "./matrixUtils";
 import { getBoardAnimationCounterClockwiseRotaion, getBoardAnimationHorizontalFlip } from "./AnimationUtils";
-
-// const getRowAnimationLeftSwipe = (row: number[]): AnimationPlan => {
-
-//     const currentTiles: MovingTile[] = [];
-
-//     const q: number[] = [];
-//     let lastTile = { value: 0, lastIndex: -1 };
-
-//     for (let index = 0; index < row.length; index++) {
-
-//         const current = row[index];
-
-//         if (current === 0)
-//             continue;
-
-//         if (current === lastTile.value) {
-//             currentTiles.push(
-//                 {
-//                     value: current,
-//                     from: { row: 0, col: index },
-//                     to: { row: 0, col: q.length }
-//                 },
-
-//                 {
-//                     value: current,
-//                     from: { row: 0, col: lastTile.lastIndex },
-//                     to: { row: 0, col: q.length }
-//                 }
-//             );
-//             q.push(current * 2);
-//             lastTile = { value: 0, lastIndex: -1 };
-//             continue;
-//         }
-
-//         if (lastTile.value !== 0) {
-//             currentTiles.push(
-//                 {
-//                     value: lastTile.value,
-//                     from: { row: 0, col: lastTile.lastIndex },
-//                     to: { row: 0, col: q.length }
-//                 },);
-//             q.push(lastTile.value);
-//         }
-
-//         lastTile = { value: current, lastIndex: index };
-
-//     }
-
-//     if (lastTile.value !== 0) {
-//         currentTiles.push(
-//             {
-//                 value: lastTile.value,
-//                 from: { row: 0, col: lastTile.lastIndex },
-//                 to: { row: 0, col: q.length }
-//             },);
-//         q.push(lastTile.value);
-//     }
-
-//     while (q.length !== row.length) {
-//         q.push(0);
-//     }
-
-//     return {
-//         movingTiles: currentTiles,
-//         staticTiles: [],
-//         mergedTiles:[]
-//         //Implement merged
-//     };
-// }
-
 
 export const getBoardAnimationLeftSwipe = (board: number[][]): AnimationPlan => {
     const finalPlan: AnimationPlan = {
@@ -82,7 +12,6 @@ export const getBoardAnimationLeftSwipe = (board: number[][]): AnimationPlan => 
 
         const row = getRow(board, rowIndex);
         const rowPlan = getRowTilesAfterLeftSwipe(row).newPlan;
-        // const rowPlan = getRowAnimationLeftSwipe(row);
         const rowMovingTiles = rowPlan.movingTiles;
 
         for (let index = 0; index < rowMovingTiles.length; index++) {
@@ -104,42 +33,24 @@ export const getBoardAnimationLeftSwipe = (board: number[][]): AnimationPlan => 
             rowMergedTiles[index].position.row = rowIndex;
             finalPlan.mergedTiles.push(rowMergedTiles[index]);
         }
-
-        //         const rowMovement = rowPlan.movingTiles;
-
-        // for (let index = 0; index < rowMovement.length; index++) {
-        //     rowMovement[index].from.row = rowIndex;
-        //     rowMovement[index].to.row = rowIndex;
-        //     finalPlan.movingTiles.push(rowMovement[index]);
-        // }
-
     }
-    // console.log("merged: ",finalPlan.mergedTiles);
     return finalPlan;
 }
 
 
 export const getBoardAnimationRightSwipe = (board: number[][]): AnimationPlan => {
-
     const plan = getBoardAnimationLeftSwipe(horizontalBoardFlip(board));
-
     return getBoardAnimationHorizontalFlip(plan, board.length);
 }
 
 
 export const getBoardAnimationUpSwipe = (board: number[][]): AnimationPlan => {
-
     const plan = getBoardAnimationRightSwipe(clockwiseBoardRotation(board));
-
     return getBoardAnimationCounterClockwiseRotaion(plan, board.length);
-
 }
 
 
 export const getBoardAnimationDownSwipe = (board: number[][]): AnimationPlan => {
-
     const plan = getBoardAnimationLeftSwipe(clockwiseBoardRotation(board));
-
     return getBoardAnimationCounterClockwiseRotaion(plan, board.length);
-
 }
