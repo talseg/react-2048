c445ae6c6760eb388afbc6b330cc718a36c79115
Tal Segal <talseg7@gmail.com>
2025-10-28 17:17:01 +0200

    Updated Storybook to version 10

--- src/game/hooks/SwipeTester.stories.ts
+++ src/game/hooks/SwipeTester.stories.ts
@@ -1,6 +1,6 @@
 import type { Meta, StoryObj } from '@storybook/react-vite';
 import { SwipeTester } from './SwipeTester';
-import { fn } from 'storybook/internal/test';
+import { fn } from 'storybook/test';
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 const meta = {
